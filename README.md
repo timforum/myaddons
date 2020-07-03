8949c47b0e8f5210000b0b12d943d1d555c9002d
Tim Meng <mengshihua@gmail.com>
2020-07-03 20:03:18 +0800

    ok

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 test
 
+git remote add origin https://github.com/timforum/odoo10Test.git
+git push -u origin master
+
 OK
--- cn_invoice/models/models.py
+++ cn_invoice/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api,exceptions
+
 
 class invoicergister(models.Model):
     _name = 'cn.invoice'
@@ -8,35 +9,49 @@ class invoicergister(models.Model):
 
     invoice_type = fields.Selection([('a','电子发票'), ('b','纸质普通发票'),('c','纸质专用发票'),('d','收据')], required=True )
     customer_net_id = fields.Char(string="Customer Net ID", required=True)
-    order_no=fields.Char( string ='Order No', required=True)
+    order_no=fields.Char( string ='Order No')
     to_company = fields.Char(string="To Company", required=True)
     tax_id = fields.Char(string="Tax ID", required=True)
+
+    # specified invoice needed informaiton
+    bank_id = fields.Char(string="Bank Account ID")
+    bank_name = fields.Char(string="Bank Name")
+    address = fields.Char(string="Address")
+    telephone = fields.Char(string="Telephone")
+
     note = fields.Text(string="Note")
     splited = fields.Boolean(string="Splited?", required=True)
     email = fields.Char(string="Email")
 
-    state = fields.Selection( [('draft','Draft'),('done','Done')],default='draft')
+    state = fields.Selection( [('draft','Draft'),('apply_invoice', 'apply_invoice'),('done','Done')],default='draft')
 
     order_line= fields.One2many("cn.orderline","cn_invoice_id",string='sku')
+    content_line =fields.One2many("cn.content","cn_invoice_id",string='sku')
 
-    def action_draft(self):
-        self.state = 'draft'
+    def action_submit(self):
+        self.state = 'apply_invoice'
 
-    def action_confirm(self):
+    def action_apply(self):
         self.state = 'done'
 
+    # def action_confirm(self):
+    #     self.state = 'done'
 
     def action_reset(self):
-       self.state = 'Reset to draft'
+       self.state = 'draft'
 
-
-
-
+    # disable delete record on state done
+    def unlink(self):
+        for order in self:
+            if self.state =='done':
+                raise exceptions.UserError('You cannot Delete this record')
+        return super().unlink()
 
 
 class cn_orderline(models.Model):
     _name = 'cn.orderline'
     _description = 'Order Line'
+    name=fields.Char(string="Order Line")
     sku = fields.Char(string="SKU")
     product_name = fields.Char(string="Product_Name",required=True)
     qty = fields.Float(string="Qty", required=True)
@@ -50,19 +65,18 @@ class cn_orderline(models.Model):
             r.amount = r.unit_price * r.qty
 
 
-class cn_content(models.Model):
-    _name = 'invoice_content'
+class cn_Content(models.Model):
+    _name = 'cn.content'
     _description = 'Invoice Content'
-    product_name = fields.Char(string="Product_Name",required=True)
+    name =fields.Char(string="Invoice Content")
+    sku = fields.Char(string="SKU")
+    product_name = fields.Char(string="Product_Name", required=True)
     qty = fields.Float(string="Qty", required=True)
-    unit_price = fields.Float(string="Unit_price",required="True")
-    # amount = fields.Float(string="Amount", compute="_amount_")
-    # cn_invoice_id = fields.Many2one("cn.invoice",string="to_company" )
+    unit_price = fields.Float(string="Unit_price", required=True)
+    amount = fields.Float(string="Amount", compute="_amount")
+    cn_invoice_id = fields.Many2one("cn.invoice")
 
-    #
-    # @api.depends(qty,unit_price)
-    # def _amount_(self):
-    #     for r in self:
-    #         r.amount = r.unit_price * r.qty
-
-
+    @api.depends('qty', 'unit_price')
+    def _amount(self):
+        for r in self:
+            r.amount = r.unit_price * r.qty
